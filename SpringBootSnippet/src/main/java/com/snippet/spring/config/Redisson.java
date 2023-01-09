0a4ae7fecc8ec0c987a0c42dad66121676b3ec5c
haiah-w <103855390+haiah-w@users.noreply.github.com>
2023-01-09 15:36:03 +0800

    Create Redisson.java

---
+++ SpringBootSnippet/src/main/java/com/snippet/spring/config/Redisson.java
@@ -0,0 +1,25 @@
+package com.snippet.spring.config;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        // 集群
+        // config.useClusterServers().addNodeAddress("172.27.172.54:6379", "");
+        // 单机
+        config.useSingleServer()
+            .setAddress("redis://192.168.86.177:6379")
+            .setDatabase(1)
+            .setConnectionPoolSize(64)
+            .setConnectionMinimumIdleSize(24)
+            .setConnectTimeout(10000)
+            .setIdleConnectionTimeout(10000)
+            .setRetryAttempts(3)
+            .setRetryInterval(1500);
+        config.setLockWatchdogTimeout(30 * 1000);   // 续锁的看门狗线程，检查锁的超时时间
+
+        return Redisson.create(config);
+    }
+}
