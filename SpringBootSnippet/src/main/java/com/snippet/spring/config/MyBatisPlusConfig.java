2789a3c89040ad0642e77931760422e785297a59
haiah-w <103855390+haiah-w@users.noreply.github.com>
2023-01-28 16:52:48 +0800

    Create MyBatisPlusConfig.java

---
+++ SpringBootSnippet/src/main/java/com/snippet/spring/config/MyBatisPlusConfig.java
@@ -0,0 +1,13 @@
+package com.snippet.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public EasySqlInjector easySqlInjector() {
+        return new EasySqlInjector();
+    }
+}
