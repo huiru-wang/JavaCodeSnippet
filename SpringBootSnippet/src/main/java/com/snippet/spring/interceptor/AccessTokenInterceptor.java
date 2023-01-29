3f5df98f299333e514614ff5c2567f050f24c09a
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-01-29 20:19:20 +0800

    Update AccessTokenInterceptor.java

--- SpringBootSnippet/src/main/java/com/snippet/spring/interceptor/AccessTokenInterceptor.java
+++ SpringBootSnippet/src/main/java/com/snippet/spring/interceptor/AccessTokenInterceptor.java
@@ -6,9 +6,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AccessTokenInterceptor implements HandlerInterceptor {
+    @Value("${jwt.private.key}")
+    private String jwtPrivateKey;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         // TODO jwt
+        String authorization = request.getHeader("Authorization");
+        Assert.notNull(authorization, "Authorization is null");
+        String[] value = authorization.split(" ");
+        Assert.isTrue(value.length > 1, "Invalid Authorization");
+        String token = value[1];
+        boolean res = JWTUtil.verify(token, jwtSecret.getBytes());
+        if (!res) {
+            log.warn("Verify JWT fail");
+            return false;
+        }
+        JWT jwt = JWTUtil.parseToken(token);
+        JWTPayload payload = jwt.getPayload();
+        Long userId = (Long) payload.getClaim("userId");
+        // verify user
+
         return true;
     }
 }
