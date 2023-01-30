df34155f8430262c718277860dcae901d616236f
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-01-30 10:22:13 +0800

    Update ResponseEnums.java

--- SpringBootSnippet/src/main/java/com/snippet/spring/common/enums/ResponseEnums.java
+++ SpringBootSnippet/src/main/java/com/snippet/spring/common/enums/ResponseEnums.java
@@ -1,13 +1,15 @@
 package com.snippet.spring.common.enums;
 
 import lombok.Getter;
-
+/**
+* 错误码：模块 | 编号 | 类型
+*/
 @Getter
 public enum ResponseEnums {
 
     SUCCESS(0, "success"),
     PARAM_INVALID(10001, "param invalid"),
-AUTHENTICATION_FAIL(10010, "authentication fail"),
+    AUTHENTICATION_FAIL(10010, "authentication fail"),
     // 用户相关
     USER_INFO_UPDATE_FAIL(20001, "user info update fail"),
     ;
