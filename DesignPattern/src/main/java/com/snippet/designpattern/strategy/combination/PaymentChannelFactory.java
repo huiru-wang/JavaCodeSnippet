567d9aa40ff6acc4d56d4ead339d4e1a968c08b4
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-01-31 10:11:43 +0800

    Update PaymentChannelFactory.java

--- DesignPattern/src/main/java/com/snippet/designpattern/strategy/combination/PaymentChannelFactory.java
+++ DesignPattern/src/main/java/com/snippet/designpattern/strategy/combination/PaymentChannelFactory.java
@@ -8,8 +8,12 @@ public class PaymentChannelFactory {
     private static final Map<String, PaymentStrategy> paymentStrategyMap = new ConcurrentHashMap<>(16);
 
     static {
-        // 初始化加载所有PaymentChannel
-        Class<? super PaymentStrategy> superclass = PaymentStrategy.class.getSuperclass();
+        // 初始化加载所有PaymentChannel，可以借助Spring容器获取接口的实现，这里简单单例处理
+        AdyenPayment adyenPayment = AdyenPayment.newInstance();
+        // 获取注解支付方式
+        PaymentChannel paymentChannel = adyenPayment。getClass().getAnnotation(PaymentChannel.class);
+        
+        paymentStrategyMap.put(paymentChannel.channel(), adyenPayment);
     }
 
     public static PaymentStrategy getPaymentStrategy(String channel){
