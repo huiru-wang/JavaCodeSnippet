a6f27c8f3d1cceb8e20e64ee337c45799a8864db
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-02-20 20:16:23 +0800

    Create UserModel.java

---
+++ JavaSnippet/src/main/java/com/snippet/Object/UserModel.java
@@ -0,0 +1,22 @@
+public class UserModel {
+
+    private Long userId;
+
+    private String username;
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public UserModel() {
+    }
+
+    public UserModel(Long userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
