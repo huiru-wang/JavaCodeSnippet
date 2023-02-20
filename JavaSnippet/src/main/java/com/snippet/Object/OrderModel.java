4ee3825eeab7f6a28e01ed54a864f1f759cfac87
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-02-20 20:16:34 +0800

    Create OrderModel.java

---
+++ JavaSnippet/src/main/java/com/snippet/Object/OrderModel.java
@@ -0,0 +1,14 @@
+public class OrderModel {
+
+    private Long orderId;
+
+    private UserModel userInfo;
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setUserInfo(UserModel userInfo) {
+        this.userInfo = userInfo;
+    }
+}
