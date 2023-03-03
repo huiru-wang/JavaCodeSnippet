9cd25a3c5e89ad0f610c774cf0521667a678647a
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-03-03 17:21:01 +0800

    Update UserService.java

--- SpringBootSnippet/src/main/java/com/snippet/spring/service/UserService.java
+++ SpringBootSnippet/src/main/java/com/snippet/spring/service/UserService.java
@@ -16,5 +16,41 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService extends ServiceImpl<UserMapper, User> implements IService<User> {
+    @Transactional(rollbackFor = Exception.class)
+    public void insertOne() {
+        PublicAlert publicAlert = new PublicAlert();
+        publicAlert.setId(IdUtil.getSnowflakeNextId());
+        publicAlert.setSite("xxxx");
+        publicAlert.setApplicationId("xxxx");
+        publicAlert.setServiceId("xxxx");
+        publicAlert.setTenantId("xxxx");
+        publicAlert.setPublicAlertName("xxx");
+        publicAlert.setStartTime(LocalDateTime.now());
+        publicAlert.setCategory("OS");
+        publicAlertMapper.insert(publicAlert);
+        int s = 1 / 0;
+    }
 
+    @Autowired
+    DataSourceTransactionManager transactionManager;
+
+    public void insertOneCode() {
+        TransactionStatus transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            PublicAlert publicAlert = new PublicAlert();
+            publicAlert.setId(IdUtil.getSnowflakeNextId());
+            publicAlert.setSite("xxxx");
+            publicAlert.setApplicationId("xxxx");
+            publicAlert.setServiceId("xxxx");
+            publicAlert.setTenantId("xxxx");
+            publicAlert.setPublicAlertName("xxx");
+            publicAlert.setStartTime(LocalDateTime.now());
+            publicAlert.setCategory("OS");
+            publicAlertMapper.insert(publicAlert);
+            // int s = 1 / 0;
+            transactionManager.commit(transactionStatus);
+        } catch (Exception e) {
+            transactionManager.rollback(transactionStatus);
+        }
+    }
 }
