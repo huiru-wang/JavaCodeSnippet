4aac0b767635a5ad8e68ed13783823f5bee17779
huiru-wang <103855390+huiru-wang@users.noreply.github.com>
2023-03-03 16:30:27 +0800

    Update KafkaListenerErrorHandler.java

--- Springkafka/src/main/java/com/snippet/kafkaexample/config/KafkaListenerErrorHandler.java
+++ Springkafka/src/main/java/com/snippet/kafkaexample/config/KafkaListenerErrorHandler.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @Configuration
 public class KafkaListenerErrorHandler {
 
+    @Bean
     public ConsumerAwareErrorHandler consumerAwareErrorHandler() {
         return (exception, consumerRecord, consumer) -> {
             if (Objects.nonNull(consumerRecord)) {
